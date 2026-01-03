34df3adb8feecabacadff20c97f644fa738c826a
Priyanshu <Preyanshukandari@gmail.com>
2026-01-04 02:11:34 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <>
+    <div id="parent">
+      <h1 id="child1">Hello</h1>
+      <h1 id="child2">Priyanshu</h1>
+    </div>
+    <div id="parent2">
+      <h1>Parent2 ke bacche</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
