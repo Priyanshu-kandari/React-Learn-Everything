b3839aa9d3d5925a2bf44f5825d1deac4e1f5aa4
Priyanshu <Preyanshukandari@gmail.com>
2026-01-14 23:49:44 +0530

    14 notes app completely done

--- 13-notes-app/src/Right.jsx
+++ 13-notes-app/src/Right.jsx
@@ -1,24 +1,37 @@
-const Right = ({deleteNote,notes}) => {
+const Right = ({ deleteNote, notes }) => {
   return (
     <div className="lg:w-2/3 flex flex-col py-10 gap-10 border-2">
-        <h1 className="self-center font-bold text-3xl">RECENT NOTES</h1>
-        <div className="flex flex-wrap gap-10 justify-center h-full overflow-auto">
-          {notes.map((note)=>{
-            return (
-              <div key={note.id} className="flex justify-between flex-col h-72 w-60 text-black rounded bg-cover px-4 py-8 pb-4 bg-[url(https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png)] ">
-                <div className="overflow-hidden">
-                  <h2 className="whitespace-nowrap overflow-x-auto text-2xl font-bold ">{note.title}</h2>
-                <div className="text-gray-900 border-solid border border-gray-800 px-2 py-1 font-medium mt-3 max-h-39 overflow-y-auto no-scrollbar wrap-break-words">{note.content}</div>
-                </div>
-                <button className="active:scale-90 w-2/3 self-center rounded font-bold  bg-red-600 cursor-pointer text-white" onClick={()=>{
-                  deleteNote(note.id)
-                }}>Delete Note</button>
+      <h1 className="self-center font-bold text-3xl">
+        RECENT NOTES
+      </h1>
+
+      <div className="flex flex-wrap justify-center gap-10 h-full overflow-auto">
+        {notes.map((note) => (
+          <div
+            key={note.id}
+            className="flex flex-col justify-between h-[330px] w-60 rounded bg-cover bg-center px-4 py-6 text-black
+                       bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]"
+          >
+            <div className="overflow-hidden">
+              <h2 className="text-2xl font-bold truncate">
+                {note.title}
+              </h2>
+
+              <div className="mt-3 max-h-44 overflow-y-auto break-words border border-gray-800 px-2 py-1 font-medium text-gray-900">
+                {note.content}
               </div>
-            )
-          })}
-        </div>
-        
+            </div>
+
+            <button
+              className="w-2/3 self-center rounded bg-red-600 py-1 font-bold text-white cursor-pointer active:scale-90"
+              onClick={() => deleteNote(note.id)}
+            >
+              Delete Note
+            </button>
+          </div>
+        ))}
       </div>
+    </div>
   )
 }
 
